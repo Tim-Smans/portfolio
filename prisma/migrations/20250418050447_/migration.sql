cbb809612d59b80a45b86eeb86c02f6fc2e46ab6
Tim-Smans <tim.smans@outlook.com>
2025-04-18 08:05:08 +0300

    updated migration

--- prisma/migrations/20250418050447_/migration.sql
+++ prisma/migrations/20250418050447_/migration.sql
@@ -2,11 +2,10 @@
   Warnings:
 
   - A unique constraint covering the columns `[slug]` on the table `Project` will be added. If there are existing duplicate values, this will fail.
-  - Added the required column `slug` to the `Project` table without a default value. This is not possible if the table is not empty.
 
 */
 -- AlterTable
-ALTER TABLE "Project" ADD COLUMN     "slug" VARCHAR(255) NOT NULL;
+ALTER TABLE "Project" ADD COLUMN     "slug" VARCHAR(255);
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Project_slug_key" ON "Project"("slug");
